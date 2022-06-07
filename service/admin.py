c54f47af4d445519d9da862907d469fa25ba86ac
Shamil Takaev <Shamilchechya@gmail.com>
2022-06-07 16:28:40 +0300

    Before Postgres DB

--- service/admin.py
+++ service/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 
-from service.models import Bin, Product
+from service.models import Bin, Product, Home
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(Bin)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "id", "cost", "is_active"]
+
+    fields = ["id", "title", "is_active"]
+    list_filter = ["id", "cost"]
+    search_fields = ["title", "id"]
+
+@admin.register(Home)
+class HomeAdmin(admin.ModelAdmin):
+    list_display = ["size", "cost", "addr", "bal"]
--- service/models.py
+++ service/models.py
@@ -2,16 +2,29 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=200, null=True)
     cost = models.CharField(max_length=200, null=True)
+    is_active = models.BooleanField(verbose_name="Активен", default=True)
 
     def __str__(self) -> str:
         return self.title 
 
 class Bin(models.Model):
+    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=200, null=True)
     cost = models.CharField(max_length=200, null=True)
 
     def __str__(self) -> str:
         return self.title 
     
+
+class Home(models.Model):
+    id = models.IntegerField(primary_key=True)
+    size = models.DecimalField(verbose_name="Размер", null = True, decimal_places=2, max_digits=10)
+    cost = models.IntegerField(verbose_name="Цена", null=True)
+    addr = models.CharField(verbose_name="Адрес", default="Грозный", max_length=200)
+    bal = models.BooleanField(verbose_name="Балкон", default=True)
+
+    def __str__(self) -> str:
+        return self.addr
